4ca670a6f5857afcaefd917f9853c4af7edbf55b
Anthony Magner <anthony.magner@gmail.com>
2021-11-10 15:54:16 -0700

    asdf

--- vars/git_helper.groovy
+++ vars/git_helper.groovy
@@ -2,9 +2,24 @@ def getRemoteHEAD(url, branch)
 {
 	def cmd = "git ls-remote ${url} ${branch}"
 	//print("cmd = ${cmd}")
-	def output = cmd.execute().text
+	def output = cmd.execute().text.trim()
 	//print("output = ${output}")
 	return output
 }
+
+def getLocalHEAD()
+{
+	def cmd = "git rev-parse HEAD"
+	def output = cmd.execute().text.trim()
+	return output
+}
+
+def getModifiedFiles(commit_id)
+{
+	def cmd = "git diff --name-only ${commit_id}"
+	def output = cmd.execute().text.trim()
+	return output
+}
+
 
 return this
