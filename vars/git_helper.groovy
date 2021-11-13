11f3f0f035ce6f36b4775d9439a7e8d2080cda39
Anthony Magner <anthony.magner@gmail.com>
2021-11-13 15:43:31 -0700

    dev

--- vars/git_helper.groovy
+++ vars/git_helper.groovy
@@ -12,19 +12,20 @@ def getRemoteBranches(url)
 	def cmd = "git ls-remote ${url} refs/heads/*"
 	def output = cmd.execute().text.trim()
 	def result = []
-	print("Output raw: ${output}")
+	//print("Output raw: ${output}")
 	def output_lines = output.split('\n')
 	//print ("Output Lines: ${output_lines}")
 	output_lines.each { line ->
 		//print("Line: ${line}")
 		def line_split = line.split('\t')
-		print("Line Split: ${line_split}")
+		//print("Line Split: ${line_split}")
 
 		if (line_split.size() == 2)
 		{
 			def ref_name =line_split[1]
 			def ref_name_split = ref_name.split('/')
-			if (ref_name_split == 2)
+			print("Ref Name Split: ${ref_name_split}")
+			if (ref_name_split.size() == 2)
 			{
 				result.add(ref_name_split[2])
 			}
