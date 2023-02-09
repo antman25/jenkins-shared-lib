ae1bea5bd6ce711d45a9bf2cfe585046ce15480c
Anthony Magner <anthony.magner@gmail.com>
2023-02-08 22:14:33 -0700

    sdfgsfd

--- vars/createBuildJobs.groovy
+++ vars/createBuildJobs.groovy
@@ -10,6 +10,12 @@ def call() {
             checkout scm
         }
 
+        stage('debug')
+        {
+            sh 'find .'
+        }
+
+
         stage ('Job DSL') {
             def params = ['workspace': "${WORKSPACE}"]
             jobDsl targets: ['seed_jobs/main.groovy'].join('\n'),
