3de5c5ee0409c5f7e93708f0ecd9991c12d86c96
Anthony Magner <anthony.magner@gmail.com>
2023-02-08 23:13:19 -0700

    sdfgsfd

--- seed_jobs/main.groovy
+++ seed_jobs/main.groovy
@@ -1,25 +1,23 @@
 import org.yaml.snakeyaml.Yaml
 
 
-void printConfig(config_data)
+void buildJobs(config_data)
 {
     if (config_data.containsKey('groups') == true)
     {
         def groups = config_data['groups']
-        //println("Groups: ${groups}")
-
         groups.each { cur_group ->
-            //println("CurGroups: ${current_group}")
             def name = cur_group.get('name')
             def project_list = cur_group.get('project_list')
             if (name != null)
             {
                 println("GroupName: ${name}")
-
+                folder("${name}")
                 if (project_list != null)
                 {
                     project_list.each { cur_project ->
                         println("Project: ${cur_project}")
+                        folder("${name}/${cur_project}")
                     }
                 }
                 else
@@ -31,10 +29,6 @@ void printConfig(config_data)
             {
                 println("No group name")
             }
-
-
-
-            //println("Project List: ${project_list}")
         }
     }
     else
@@ -46,6 +40,6 @@ void printConfig(config_data)
 }
 
 def config_yaml = new Yaml().load(config)
-printConfig(config_yaml)
-println("YAML: ${config_yaml}")
+buildJobs(config_yaml)
+
 
