02e2058ba719cd4af71d8c2e8c5b72a34aa40b46
Anthony Magner <anthony.magner@gmail.com>
2023-02-08 22:30:39 -0700

    sdfgsfd

--- seed_jobs/main.groovy
+++ seed_jobs/main.groovy
@@ -5,7 +5,7 @@ folder('test folder2')
 
 println("Workspace: ${workspace}")
 
-def config_path = ${workspace}/seed_jobs/config.yaml".toString()
+def config_path = "${workspace}/seed_jobs/config.yaml".toString()
 def config_raw = readFile(config_path)
 
 println("Config Raw: ${config_raw}")
