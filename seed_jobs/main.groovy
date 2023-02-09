a56356c8ef30a89d069647e9d1eb0c938bc1061e
Anthony Magner <anthony.magner@gmail.com>
2023-02-08 23:01:49 -0700

    sdfgsfd

--- seed_jobs/main.groovy
+++ seed_jobs/main.groovy
@@ -1,15 +1,29 @@
 import org.yaml.snakeyaml.Yaml
 
-folder('test folder')
-folder('test folder2')
-
-println("Workspace: ${workspace}")
-
-//def config_path = "${workspace}/seed_jobs/config.yaml".toString()
-
-println("Config Raw: ${config}")
-
 def config_data = new Yaml().load(config)
+
+void printConfig()
+{
+    if (config_data.containsKey('groups') == true)
+    {
+        def groups = config_data['groups']
+        println("Groups: ${groups}")
+
+        groups.each { current_group ->
+            println("CurGroups: ${current_group}")
+            def name = current_group.get('name')
+            def project_list = current_group.get('project_list')
+            println("Group: ${name}")
+            println("Project List: ${project_list}")
+        }
+    }
+    else
+    {
+        println("Missing groups key")
+    }
+
+
+}
 
 
 println("YAML: ${config_data}")
