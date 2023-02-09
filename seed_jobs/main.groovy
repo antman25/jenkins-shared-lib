2165e5ae041f0f6d731f14e300fc14f8a3a6abe8
Anthony Magner <anthony.magner@gmail.com>
2023-02-08 21:52:27 -0700

    sdfgsfd

--- seed_jobs/main.groovy
+++ seed_jobs/main.groovy
@@ -3,5 +3,5 @@ import org.yaml.snakeyaml.Yaml
 folder('test folder')
 folder('test folder2')
 
-def yaml = new org.yaml.snakeyaml.Yaml(new FileReader('config.yaml'))
+def yaml = new org.yaml.snakeyaml.Yaml(new FileReader('seed_jobs/config.yaml'))
 println("YAML: ${yaml}")
