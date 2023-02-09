1b8eb0dbb1d0c623e1382642982bb0e880c6034d
Anthony Magner <anthony.magner@gmail.com>
2023-02-08 22:00:39 -0700

    sdfgsfd

--- seed_jobs/main.groovy
+++ seed_jobs/main.groovy
@@ -3,5 +3,8 @@ import org.yaml.snakeyaml.Yaml
 folder('test folder')
 folder('test folder2')
 
-def yaml = new org.yaml.snakeyaml.Yaml(new FileReader("${WORKSPACE}/seed_jobs/config.yaml"))
+def env_vars = env.getEnvironment()
+println("Env Vars: ${env_vars}")
+
+def yaml = new org.yaml.snakeyaml.Yaml(new FileReader("..//seed_jobs/config.yaml"))
 println("YAML: ${yaml}")
