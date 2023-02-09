51608fb99a7cba25b487fe0a48b3d8de64c3c021
Anthony Magner <anthony.magner@gmail.com>
2023-02-08 22:27:39 -0700

    sdfgsfd

--- seed_jobs/main.groovy
+++ seed_jobs/main.groovy
@@ -5,9 +5,10 @@ folder('test folder2')
 
 println("Workspace: ${workspace}")
 
-def config_str = readFile("${workspace}/seed_jobs/config.yaml")
+def config_path = ${workspace}/seed_jobs/config.yaml".toString()
+def config_raw = readFile(config_path)
 
-println("Config Raw: ${config_str}")
+println("Config Raw: ${config_raw}")
 //def yaml = new org.yaml.snakeyaml.Yaml(new FileReader("${workspace}/seed_jobs/config.yaml"))
 //println("YAML: ${yaml}")
 
