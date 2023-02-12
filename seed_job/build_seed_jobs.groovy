4bc836c76eac114832d7b07e89baca747b67f41e
Anthony Magner <anthony.magner@gmail.com>
2023-02-11 21:52:12 -0700

    k8s agent added

--- seed_job/build_seed_jobs.groovy
+++ seed_job/build_seed_jobs.groovy
@@ -2,6 +2,12 @@ folder(seed_jobs_root)
 {
     displayName("Seed Jobs")
     description("Automated deployment of jenkins jobs")
+
+    properties {
+        authorizationMatrix {
+            inheritanceStrategy { nonInheriting() }
+        }
+    }
 }
 
 folder("${seed_jobs_root}/job_testing")
