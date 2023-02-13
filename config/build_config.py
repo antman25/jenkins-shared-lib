281fd72f07661c58147adfbd6cfedf46a467f013
Anthony Magner <anthony.magner@gmail.com>
2023-02-12 21:13:56 -0700

    testin

--- config/build_config.py
+++ config/build_config.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 import yaml
 
-with open('config/config_test.yaml', 'w') as f:
-    f.write('test')
+def setGlobalVar(config, var_name, val)
+    if 'global' not in config:
+        config['global'] = {}
+    config['global'][var_name] = val
+
+def main():
+    output_path = 'config/config.yaml'
+    output_config = {}
+
+    setGlobalVar('artifactory_url', 'http://nexus.antlinux.local')
+    setGlobalVar('bitbucket_url', 'http://bitbucket.antlinux.local')
+
+    with open('config/config_test.yaml', 'w') as f:
+        yaml.dump(output_config, f)
+
+
+if __name__ == '__main__':
+    main()
