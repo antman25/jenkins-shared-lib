4ea75d78fbbdd3e407a4a750ef96a26ee2e2c069
Anthony Magner <anthony.magner@gmail.com>
2023-02-12 21:48:23 -0700

    testin

--- config/build_config.py
+++ config/build_config.py
@@ -8,23 +8,29 @@ def setGlobalVar(config, var_name, val):
 
 
 def createTenantDefaults (tenant_name, perm_groups, project_list):
-    if perm_groups is not list:
-        raise Exception('Invalid permission group type passed' + perm_groups)
-    if project_list is not list:
+    if type(perm_groups) != list:
+        raise Exception('Invalid permission group type passed')
+    if type(project_list) != list:
         raise Exception('Invalid project list type passed')
     tenant_config = { 'tenant_name' : tenant_name,
-                          'perm_groups' : perm_groups,
-                          'project_list' : project_list
-                        }
+                      'perm_groups' : perm_groups,
+                      'project_list' : project_list
+                    }
     return tenant_config
+
+def addTentant(tenants, tenant_name, perm_groups, project_list):
+    tenant_config = createTenantDefaults(tenant_name, perm_groups, project_list)
+    print("Adding Tentant: %s" % tenant_name)
+    tenants.append(tenant_config)
 
 def createAllTenants(config):
     if 'tenants' not in config:
         config['tenants'] = []
-    config['tenants'].append(createTenantDefaults('tenA', ['groupA'], ['prjA']))
-    config['tenants'].append(createTenantDefaults('tenB', ['groupA', 'groupB'], ['prjA']))
-
-
+    addTentant(config['tenants'], 'tenA', ['groupA'], ['prjA'])
+    addTentant(config['tenants'], 'tenB', ['groupA', 'groupB'], ['prjB'])
+    addTentant(config['tenants'], 'tenC', ['groupC'], ['prjC'])
+    addTentant(config['tenants'], 'tenD', ['groupD'], ['prjD'])
+    addTentant(config['tenants'], 'tenE', ['groupE'], ['prjE', 'prjF'])
 
 def main():
     output_path = 'config/config.yaml'
