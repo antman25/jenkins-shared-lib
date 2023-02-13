e6f56a8bf92ce7b47bf385f5e2cae3c7970ddc6a
Anthony Magner <anthony.magner@gmail.com>
2023-02-12 21:28:22 -0700

    testin

--- config/build_config.py
+++ config/build_config.py
@@ -6,12 +6,35 @@ def setGlobalVar(config, var_name, val)
         config['global'] = {}
     config['global'][var_name] = val
 
+
+def createTenantDefaults (tenant_name, perm_groups, project_list)
+    if perm_groups is str:
+        perm_groups = [perm_groups]
+    if project_list is str:
+        project_list = [project_list]
+    if perm_groups is not list:
+        raise Exception('Invalid permission group type passed')
+    if project_list is not list:
+        raise Exception('Invalid project list type passed')
+    def tenant_config = { 'tenant_name' : tenant_name,
+                          'perm_groups' : perm_groups,
+                          'project_list' : project_list
+                        }
+    return tenant_config
+
+def createAllTenants(config)
+    if 'tenants' not in config:
+        config['tenants'] = []
+    config['tenants'].append(createTenantDefaults('tenA', 'groupA', 'prjA'))
+
+
 def main():
     output_path = 'config/config.yaml'
     output_config = {}
 
     setGlobalVar(output_config, 'artifactory_url', 'http://nexus.antlinux.local')
     setGlobalVar(output_config, 'bitbucket_url', 'http://bitbucket.antlinux.local')
+    createAllTenants(output_config)
 
     with open('config/config_test.yaml', 'w') as f:
         yaml.dump(output_config, f)
