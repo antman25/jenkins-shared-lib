085e48d88376a3f2e922372e76c8884a9ca6476e
Anthony Magner <anthony.magner@gmail.com>
2023-02-12 19:10:07 -0700

    testin

--- job-automation/dsl/smoketest/create_smoketest_jobs.groovy
+++ job-automation/dsl/smoketest/create_smoketest_jobs.groovy
@@ -64,7 +64,6 @@ boolean createSmoktestTemplatePython(String path_prefix)
 boolean main()
 {
     String path_prefix = getPathPrefix(branch_name, delivery_branch)
-    def test = utils.getConfig()
     boolean create_smoketest_root = createSmoktestRoot(path_prefix)
     if (create_smoketest_root) {
         println("Create Smoketest Root: SUCCESS")
