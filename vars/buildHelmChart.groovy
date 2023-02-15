6ea7993098c7d016b9529cc27356b43c9de231ca
Anthony Magner <anthony.magner@gmail.com>
2023-02-15 02:13:32 -0700

    testin

--- vars/buildHelmChart.groovy
+++ vars/buildHelmChart.groovy
@@ -11,6 +11,7 @@ def call(Map config, String chart_root_path) {
 
     container('helm') {
       dir(chart_root_path) {
+        sh 'mkdir test'
         sh 'pwd && find . && ls -latr'
         //chartProps = readYaml file: ''
         def chart_data = readFile 'Chart.yaml'
