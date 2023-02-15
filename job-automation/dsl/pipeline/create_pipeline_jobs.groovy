f8eea493eae4fce38da39d398c67f2485076e690
Anthony Magner <anthony.magner@gmail.com>
2023-02-14 18:33:37 -0700

    testin

--- job-automation/dsl/pipeline/create_pipeline_jobs.groovy
+++ job-automation/dsl/pipeline/create_pipeline_jobs.groovy
@@ -125,7 +125,7 @@ boolean createJobTestFolder(String path_prefix)
 {
     try
     {
-        folder("${path_prefix}/${pipeline_root_folder}/${job_testing_folder}")
+        folder("/${job_testing_folder}")
         {
             displayName("010 - Testing Area Root")
             description("Spot to test job dsl code prior to delivery")
