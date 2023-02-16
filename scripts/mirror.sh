04940066f00b604de73ebe317bef386e9dfb2b06
Anthony Magner <anthony.magner@gmail.com>
2023-02-15 19:50:09 -0700

    testin

---
+++ scripts/mirror.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm -rf mirror
+git clone --mirror https://github.com/antman25/jenkins-shared-lib.git mirror && \
+cd mirror && \
+git remote set-url origin ssh://git@bitbucket.antlinux.local:7999/pip/jenkins-shared-lib.git && \
+git push --mirror
