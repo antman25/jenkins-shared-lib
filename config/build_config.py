82de6de918e5733e0f90b00f2d3d4d945298c2dc
Anthony Magner <anthony.magner@gmail.com>
2023-02-18 13:44:30 -0700

    testin

--- config/build_config.py
+++ config/build_config.py
@@ -8,32 +8,31 @@ def setGlobalVar(config, var_name, val):
     config['global'][var_name] = val
 
 
-def createTenantDefaults (tenant_name, perm_groups, project_list):
+def createTenantDefaults (tenant_name, perm_groups, project_list, filter_repo_regex):
     if type(perm_groups) != list:
         raise Exception('Invalid permission group type passed')
     if type(project_list) != list:
         raise Exception('Invalid project list type passed')
     tenant_config = { 'tenant_name' : tenant_name,
                       'perm_groups' : perm_groups,
-                      'project_list' : project_list
+                      'project_list' : project_list,
+                      'filter_repo_regex' : filter_repo_regex
                     }
     return tenant_config
 
-def addTentant(tenants, tenant_name, perm_groups, project_list):
-    tenant_name = tenant_name.lower()
-    tenant_config = createTenantDefaults(tenant_name, perm_groups, project_list)
+def addTentant(tenants, tenant_name, perm_groups, project_list, filter_repo_regex = '.*'):
+    tenant_config = createTenantDefaults(tenant_name, perm_groups, project_list, filter_repo_regex)
     print("Adding Tentant: %s" % tenant_name)
     tenants.append(tenant_config)
 
 def createAllTenants(config):
     if 'tenants' not in config:
         config['tenants'] = []
-    addTentant(config['tenants'], 'Pipeline', ['pipeline'], ['PIP'])
-    addTentant(config['tenants'], 'tenA', ['groupA'], ['prjA'])
-    addTentant(config['tenants'], 'tenB', ['groupA', 'groupB'], ['prjB'])
-    addTentant(config['tenants'], 'tenC', ['groupC'], ['prjC'])
-    addTentant(config['tenants'], 'tenD', ['groupD'], ['prjD'])
-    addTentant(config['tenants'], 'tenE', ['groupE'], ['prjE', 'prjF'])
+    addTentant(config['tenants'], 'Pipeline', ['pipeline'], ['PIP'], '^(?!.*(jenkins-shared-lib))')
+    addTentant(config['tenants'], 'Group-A', ['groupA'], ['prjA'])
+    addTentant(config['tenants'], 'Group-B', ['groupA', 'groupB'], ['prjB'])
+    addTentant(config['tenants'], 'Test-1', ['groupC'], ['prjC'])
+    addTentant(config['tenants'], 'Test-2', ['groupD'], ['prjD'])
 
 def main():
     output_path = 'config/config.yaml'
