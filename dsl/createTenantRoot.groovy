b5bd80868f5e4c0fd8efa5706ac7480e79ea5e11
Anthony Magner <anthony.magner@gmail.com>
2023-02-19 12:11:41 -0700

    testin

--- dsl/createTenantRoot.groovy
+++ dsl/createTenantRoot.groovy
@@ -184,7 +184,7 @@ def templateMultibranchPipeline(String job_path, String display_name, String des
     }
     catch (Exception ex)
     {
-        println("Exception: {ex.toString()}")
+        println("Exception: {ex}")
         return false
     }
     return true
